5772942d430cde38aaa97a45b0d934df91da824d
Jjinbao <jinbaow@yeah.net>
2017-05-02 22:30:29 +0800

    11111111111

--- src/app/views/home/home.js
+++ src/app/views/home/home.js
@@ -1,12 +1,10 @@
 angular.module('app.home',[])
     .controller('swalkHome',['$scope','$http','$interval',function($scope,$http,$interval){
-      console.log('-----------');
-      //var myStr='0-123-56 23   789-56';
-      // orderString(myStr);
-      // function orderString(S){
-      //   while (S.indexOf(' ')!=-1){
-      //     S=S.replace(' ','');
-      //   }
-      //   return S;
-      // }
+        $scope.nowActive='ymy';
+        $scope.changeItem=function(val){
+            if($scope.nowActive==val){
+                return;
+            }
+            $scope.nowActive=val;
+        }
     }])
