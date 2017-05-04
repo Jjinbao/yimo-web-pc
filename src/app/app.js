2dcdaab7e18e66a5efd9a30545bdf6b129992089
王金宝 <wangjinbao@4zlink.com>
2017-05-04 18:50:06 +0800

    框架搭建

--- src/app/app.js
+++ src/app/app.js
@@ -1,33 +1,82 @@
 'use strict'
 
-angular.module('swalk', ['ngRoute','ngTouch','app.router','app.login','app.home','app.info','app.teach','app.mine'])
-    /*所有控制器的父控制器*/
-    .controller('rootTabCtrl', ['$scope','$location',function($scope,$location) {
-        $scope.activeTab='YMY'
-        $scope.clickTab=function(val){
-            if($scope.activeTab==val){
-                return;
-            }
-          $scope.activeTab=val;
+angular.module('swalk', ['ngRoute', 'ui.bootstrap', 'ngTouch', 'app.router', 'app.login', 'app.home', 'app.info', 'app.teach', 'app.mine'])
+/*所有控制器的父控制器*/
+  .controller('rootTabCtrl', ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
+    $scope.activeTab = 'YMY'
+    $scope.clickTab = function (val) {
+      if ($scope.activeTab == val) {
+        return;
+      }
+      $scope.activeTab = val;
+      if($scope.activeTab=='WD'){
+        $location.path('/mine')
+      }
+    }
+    $rootScope.windowHeight = window.document.body.clientHeight;
+  }])
+
+  //隐藏底部导航栏
+  .directive('hideTabs', function ($rootScope) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attributes) {
+        scope.$on('$ionicView.beforeEnter', function () {
+          scope.$watch(attributes.hideTabs, function (value) {
+            $rootScope.hideTabs = value;
+          });
+        });
+        scope.$on('$ionicView.beforeLeave', function () {
+          $rootScope.hideTabs = false;
+        });
+      }
+    };
+  })
+  //监听dom渲染完毕
+  .directive('renderFinish', function ($timeout) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attr) {
+        if (scope.$last === true) {
+          $timeout(function () {
+            scope.$emit('ngRepeatFinished');
+          });
         }
-    }])
+      }
+    };
+  })
+  //监听屏幕宽度和高度
+  .directive('resize', function ($window) {
+    return function (scope, element,attr) {
+      var w = angular.element($window);
+      scope.getWindowDimensions = function () {
+        return {'h': w.height(), 'w': w.width()};
+      };
+      scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+        scope.windowHeight = newValue.h;
+        scope.windowWidth = newValue.w;
+        var resultStyle={};
 
-    //隐藏底部导航栏
-    .directive('hideTabs', function($rootScope) {
-        return {
-            restrict: 'A',
-            link: function(scope, element, attributes) {
-                scope.$on('$ionicView.beforeEnter', function() {
-                    scope.$watch(attributes.hideTabs, function(value) {
-                        $rootScope.hideTabs = value;
-                    });
-                });
-                scope.$on('$ionicView.beforeLeave', function() {
-                    $rootScope.hideTabs = false;
-                });
+        scope.style = function () {
+          if(attr.panlWidth){
+            return {
+              'height': (newValue.h - 100) + 'px',
+              'width':((newValue.w<1366?1366:newValue.w)-attr.menuWidth)+'px'
             }
-        };
-    });
+          }else{
+            return {
+              'height': (newValue.h - 100) + 'px'
+            };
+          }
 
+        };
+
+      }, true);
+
+      w.bind('resize', function () {
+        scope.$apply();
+      });
+    }
+  })
 
 
--- src/app/views/home/home.js
+++ src/app/views/home/home.js
@@ -1,10 +1,15 @@
-angular.module('app.home',[])
-    .controller('swalkHome',['$scope','$http','$interval',function($scope,$http,$interval){
-        $scope.nowActive='ymy';
-        $scope.changeItem=function(val){
-            if($scope.nowActive==val){
-                return;
-            }
-            $scope.nowActive=val;
-        }
-    }])
+'use strict'
+angular.module('app.home', [])
+  .controller('swalkHome', ['$rootScope','$scope', '$http', '$interval', function ($rootScope,$scope, $http, $interval) {
+    $scope.nowActive = 'ymy';
+    $scope.changeItem = function (val) {
+      if ($scope.nowActive == val) {
+        return;
+      }
+      $scope.nowActive = val;
+    };
+    $scope.menuStyle={
+      height:$rootScope.windowHeight-100+'px'
+    }
+
+  }])
