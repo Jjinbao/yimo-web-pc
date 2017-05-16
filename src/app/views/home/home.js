db5be9a79659a66428be476c69b4fce7ee3334e6
Jjinbao <jinbaow@yeah.net>
2017-05-16 23:10:43 +0800

    视频列表页面布局，左中右的布局模式

--- src/app/views/home/home.js
+++ src/app/views/home/home.js
@@ -1,26 +1,33 @@
 'use strict'
 angular.module('app.home', [])
-  .controller('swalkApp', ['$rootScope', '$scope', '$http', '$interval','$window', function ($rootScope, $scope, $http, $interval,$window) {
-    $scope.nowActive = 'myApp';
-    $scope.changeItem = function (val) {
-      if ($scope.nowActive == val) {
-        return;
-      }
-      $scope.nowActive = val;
-    };
+    .controller('swalkApp', ['$rootScope', '$scope', '$http', '$interval', '$window', function ($rootScope, $scope, $http, $interval, $window) {
+        $scope.nowActive = 'myApp';
+        $scope.changeItem = function (val) {
+            if ($scope.nowActive == val) {
+                return;
+            }
+            $scope.nowActive = val;
+        };
 
-      var appWindow = angular.element($window);
-      $scope.getWindowDimensions = function () {
-        return {'h': appWindow.height(), 'w': appWindow.width()};
-      };
-      $scope.panelWidth = {}
-      $scope.$watchCollection($scope.getWindowDimensions, function (newVal) {
-        $scope.panelWidth = {
-          height: newVal.h - 100,
-          //width: newVal.w < 1366 ? 1116: newVal.w - 250
-          width: newVal.w - 250
-        }
-      })
+        var appWindow = angular.element($window);
+        $scope.getWindowDimensions = function () {
+            return {'h': appWindow.height(), 'w': appWindow.width()};
+        };
+        $scope.panelWidth = {}
+        $scope.$watchCollection($scope.getWindowDimensions, function (newVal) {
+            $scope.panelWidth = {
+                height: newVal.h - 100,
+                //width: newVal.w < 1366 ? 1116: newVal.w - 250
+                width: newVal.w - 250
+            }
+        })
+
+        $http({
+            url:baseUrl+'ym/app/list.api',
+            method:'POST'
+        }).success(function(data){
+            console.log(data);
+        })
 
 
-  }])
+    }])
