cc9690a4075d6c82aac8148fe6cd3e06f206d78c
王金宝 <wangjinbao@4zlink.com>
2017-08-16 11:21:35 +0800

    增加优尼克

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ var baseUrl='http://www.bjyimoyun.com/';
 var staticAppList=[
         {id:1005,name:'艾德AIDman单机版',enabled:true,status:'添加',imgSrc:'./app/img/app-adman.png',version:'3.4',intro:'AIDman Pro智能心肺复苏急救训练考核系统单机版产品，连续6年作为全国职业院校护理技能大赛官方指定赛品。',delete:false},
         {id:1007,name:'艾德AIDman网络版教师端',enabled:true,status:'添加',version:'3.4',imgSrc:'./app/img/app-adtech.png',intro:'AIDman Pro智能心肺复苏急救训练考核系统网络版产品，为高效的网络分组教学、训练、考核等使用场景定制。此为教师端软件。',delete:false},
-        {id:1008,name:'复苏小龙',enabled:false,status:'开发中',imgSrc:'./app/img/app-fusu.png',version:'1.0',intro:'复苏小龙A100心肺复苏训练模拟人，可以直观反映心肺复苏操作对供血的影响，便携、易用，为普及CPR急救知识而生。',delete:false},
         {id:1009,name:'优尼克UniqueMan',enabled:true,status:'添加',imgSrc:'./app/img/app-yunike.png',version:'3.0',intro:'优尼克UniqueMan综合仿生急救训练考核系统，是一款ALS高级生命支持训练的模拟人，通过真实的临床病例模拟，训练学生实操技能和临床思维能力。',delete:false},
+        {id:1008,name:'复苏小龙',enabled:false,status:'开发中',imgSrc:'./app/img/app-fusu.png',version:'1.0',intro:'复苏小龙A100心肺复苏训练模拟人，可以直观反映心肺复苏操作对供血的影响，便携、易用，为普及CPR急救知识而生。',delete:false},
         {id:1010,name:'智能分娩急救模拟人',enabled:false,status:'开发中',imgSrc:'./app/img/app-fm.png',version:'3.0',intro:'本系统以急救和分娩知识为主，涵盖妇科、儿科、产科、急救及护理等学科，通过临床病例模拟，培养学生操作技能和临床思维能力。',delete:false},
         {id:1011,name:'宫腔操作模拟系统',enabled:false,status:'开发中',imgSrc:'./app/img/app-gq.png',version:'3.0',intro:'基于本系统可以进行分段诊刮术和盆腔检查操作，并通过计算机对操作进行系统评估。',delete:false},
         {id:1012,name:'心肺听诊模拟人',enabled:false,status:'开发中',imgSrc:'./app/img/app-xf.png',version:'3.0',intro:'操作者可以对模拟人进行心肺听诊操作，训练听诊音的辨别能力。系统集成了训练及考核模式，以适应不同的教学场景。',delete:false}
